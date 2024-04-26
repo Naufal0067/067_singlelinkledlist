9da9c369f47200a5de777e5a122d31c05f56bff2
Naufal0067 <naufalopang1928@gmail.com>
2024-04-26 15:35:57 +0700

    menambahkan previous dan current

--- 067_singlelinkledlist/067_singlelinkledlist.cpp
+++ 067_singlelinkledlist/067_singlelinkledlist.cpp
@@ -42,5 +42,11 @@ void addNode() {
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
+		previous = current;
+		current = current->next;
+		
 	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
